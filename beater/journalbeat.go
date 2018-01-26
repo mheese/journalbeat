20207df9f5b5764cc96bf32a44bc283dc0b4e822
Mihai Petracovici <petracvv@users.noreply.github.com>
2018-01-26 16:51:19 -0600

    Fix publishing of pending events on start (#137)

--- beater/journalbeat.go
+++ beater/journalbeat.go
@@ -136,6 +136,9 @@ func (jb *Journalbeat) publishPending() error {
 
 	logp.Info("Loaded %d events, trying to publish", len(pending))
 	for cursor, event := range pending {
+		// We need to convert the timestamp back to the correct type before trying to publish
+		timestamp, _ := time.Parse(time.RFC3339, event["@timestamp"].(string))
+		event["@timestamp"] = common.Time(timestamp)
 		ref := &eventReference{cursor, event}
 		jb.pending <- ref
 		refs = append(refs, ref)
